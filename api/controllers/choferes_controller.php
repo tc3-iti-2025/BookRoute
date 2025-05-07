5bc94fa9857493b29417293c7fdb8513d56f9db6
Ignacio Tejera <itejera01@gmail.com>
2025-05-07 15:27:54 -0300

    agregado el modelo de choferes y el controlador de choferes

--- api/controllers/choferes_controller.php
+++ api/controllers/choferes_controller.php
@@ -1,14 +1,70 @@
 <?php
 
-/*
-create table choferes(
-  id int auto_increment not null,
-  persona int,
-  vencimiento_libreta date not null,
-  categoria_libreta varchar(3) not null,
-  habilitado boolean default true,
-  primary key(id),
-  foreign key (persona) references personas(id)
-);
-*/
+require_once '../models/choferes.php';
+$choferes = new choferes();
 
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'GET':
+    if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+      $result = $choferes->getChoferes($id);
+      if ($result) {
+        echo json_encode($result);
+      } else {
+        echo json_encode(array("message" => "Chofer not found."));
+      }
+    } else {
+      $result = $choferes->getChoferes();
+      echo json_encode($result);
+    }
+    break;
+  case 'POST':
+    if (isset($_POST['persona']) && isset($_POST['vencimiento_libreta']) && isset($_POST['categoria_libreta']) && isset($_POST['habilitado'])) {
+      $persona = $_POST['persona'];
+      $vencimiento_libreta = $_POST['vencimiento_libreta'];
+      $categoria_libreta = $_POST['categoria_libreta'];
+      $habilitado = $_POST['habilitado'];
+      $result = $choferes->insertChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado);
+      if ($result) {
+        echo json_encode(array("message" => "Chofer created successfully.", "id" => $result));
+      } else {
+        echo json_encode(array("message" => "Failed to create chofer."));
+      }
+    } else {
+      echo json_encode(array("message" => "Required fields not provided."));
+    }
+    break;
+  case 'PUT':
+    parse_str(file_get_contents("php://input"), $_PUT);
+    if (isset($_PUT['id']) && isset($_PUT['vencimiento_libreta']) && isset($_PUT['categoria_libreta']) && isset($_PUT['habilitado'])) {
+      $id = $_PUT['id'];
+      $vencimiento_libreta = $_PUT['vencimiento_libreta'];
+      $categoria_libreta = $_PUT['categoria_libreta'];
+      $habilitado = $_PUT['habilitado'];
+      $result = $choferes->updateChofer($id, $vencimiento_libreta, $categoria_libreta, $habilitado);
+      if ($result) {
+        echo json_encode(array("message" => "Chofer updated successfully."));
+      } else {
+        echo json_encode(array("message" => "Failed to update chofer."));
+      }
+    } else {
+      echo json_encode(array("message" => "Required fields not provided."));
+    }
+    break;
+  case 'DELETE':
+    if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+      $result = $choferes->deleteChofer($id);
+      if ($result) {
+        echo json_encode(array("message" => "Chofer deleted successfully."));
+      } else {
+        echo json_encode(array("message" => "Failed to delete chofer."));
+      }
+    } else {
+      echo json_encode(array("message" => "ID not provided."));
+    }
+    break;
+  default:
+    echo json_encode(array("message" => "Invalid request method."));
+    break;
+}
--- api/models/choferes.php
+++ api/models/choferes.php
@@ -15,13 +15,13 @@ create table choferes(
 require_once '../db/db.php';
 class choferes
 {
-  private $id;
-  private $persona;
-  private $vencimiento_libreta;
-  private $categoria_libreta;
-  private $habilitado;
+  public $id;
+  public $persona;
+  public $vencimiento_libreta;
+  public $categoria_libreta;
+  public $habilitado;
 
-  public function __construct($id, $persona, $vencimiento_libreta, $categoria_libreta, $habilitado)
+  public function __construct($id = null, $persona = null, $vencimiento_libreta = null, $categoria_libreta = null, $habilitado = null)
   {
     $this->id = $id;
     $this->persona = $persona;
@@ -30,9 +30,60 @@ class choferes
     $this->habilitado = $habilitado;
   }
 
-  // public function insertChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado)
-  // public function updateChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado)
-  // public function deleteChofer($id)
-  // public function getChoferes($id=null)
-  // public function validateChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado)
+  public function insertChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado) {
+    global $db;
+    $query = "INSERT INTO choferes (persona, vencimiento_libreta, categoria_libreta, habilitado) VALUES (?, ?, ?, ?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("issi", $persona, $vencimiento_libreta, $categoria_libreta, $habilitado);
+    if ($stmt->execute()) {
+      return $db->insert_id;
+    } else {
+      return false;
+    }
+  }
+  public function updateChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado) {
+    global $db;
+    $query = "UPDATE choferes SET vencimiento_libreta=?, categoria_libreta=?, habilitado=? WHERE persona=?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("issi", $vencimiento_libreta, $categoria_libreta, $habilitado, $persona);
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public function deleteChofer($id) {
+    global $db;
+    $query = "UPDATE choferes SET habilitado=false WHERE id=?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("i", $id);
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public function getChoferes($id = null) {
+    global $db;
+    if ($id) {
+      $query = "SELECT * FROM choferes WHERE id=? AND habilitado=true";
+      $stmt = $db->prepare($query);
+      $stmt->bind_param("i", $id);
+    } else {
+      $query = "SELECT * FROM choferes WHERE habilitado=true";
+      $stmt = $db->prepare($query);
+    }
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->fetch_all(MYSQLI_ASSOC);
+  }
+  public function validateChofer($persona, $vencimiento_libreta, $categoria_libreta, $habilitado) {
+    global $db;
+    $query = "SELECT * FROM choferes WHERE persona=? AND vencimiento_libreta=? AND categoria_libreta=? AND habilitado=?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("issi", $persona, $vencimiento_libreta, $categoria_libreta, $habilitado);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->fetch_all(MYSQLI_ASSOC);
+  }
 }
