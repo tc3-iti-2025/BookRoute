d1bd360cac3b11c53628b270e97fc0e1ad730ed2
Ignacio Tejera <itejera01@gmail.com>
2025-05-07 21:06:11 -0300

    agreados el modelo de choferes_vehiculos y su controlador

--- api/controllers/choferes_vehiculos_controller.php
+++ api/controllers/choferes_vehiculos_controller.php
@@ -1,12 +1,62 @@
 <?php
 
-/*
-create table choferes_vehiculos(
-  chofer int,
-  vehiculo int,
-  primary key (chofer, vehiculo),  
-  foreign key (chofer) references choferes(id),
-  foreign key (vehiculo) references vehiculos(id)
-);
-*/
+require_once '../models/choferes_vehiculos.php';
+$choferes_vehiculos = new choferes_vehiculos();
 
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'GET':
+    if (!isset($_GET['chofer']) && !isset($_GET['vehiculo'])) {
+      $result = $choferes_vehiculos->getChoferesVehiculos();
+    }
+    if (isset($_GET['chofer']) && !isset($_GET['vehiculo'])) {
+      $chofer = $_GET['chofer'];
+      $result = $choferes_vehiculos->getChoferesVehiculos($chofer, null);
+    }
+    if (isset($_GET['vehiculo']) && !isset($_GET['chofer'])) {
+      $vehiculo = $_GET['vehiculo'];
+      $result = $choferes_vehiculos->getChoferesVehiculos(null, $vehiculo);
+    }
+    if (isset($_GET['chofer']) && isset($_GET['vehiculo'])) {
+      $chofer = $_GET['chofer'];
+      $vehiculo = $_GET['vehiculo'];
+      $result = $choferes_vehiculos->getChoferesVehiculos($chofer, $vehiculo);
+    }
+    echo json_encode($result);
+    break;
+  case 'POST':
+    $data = json_decode(file_get_contents("php://input"));
+    if (isset($data->chofer) && isset($data->vehiculo)) {
+      $chofer = $data->chofer;
+      $vehiculo = $data->vehiculo;
+      $result = $choferes_vehiculos->insertChoferVehiculo($chofer, $vehiculo);
+      echo json_encode($result);
+    } else {
+      echo json_encode(array("error" => "Invalid input"));
+    }
+    break;
+  case 'PUT':
+    $data = json_decode(file_get_contents("php://input"));
+    if (isset($data->chofer) && isset($data->vehiculo)) {
+      $chofer = $data->chofer;
+      $vehiculo = $data->vehiculo;
+      $result = $choferes_vehiculos->updateChoferVehiculo($chofer, $vehiculo);
+      echo json_encode($result);
+    } else {
+      echo json_encode(array("error" => "Invalid input"));
+    }
+    break;
+  case 'DELETE':
+    $data = json_decode(file_get_contents("php://input"));
+    if (isset($data->chofer) && isset($data->vehiculo)) {
+      $chofer = $data->chofer;
+      $vehiculo = $data->vehiculo;
+      $result = $choferes_vehiculos->deleteChoferVehiculo($chofer, $vehiculo);
+      echo json_encode($result);
+    } else {
+      echo json_encode(array("error" => "Invalid input"));
+    }
+    break;
+  default:
+    echo json_encode(array("error" => "Invalid request method"));
+    break;
+}
--- api/models/choferes_vehiculos.php
+++ api/models/choferes_vehiculos.php
@@ -1,29 +1,76 @@
 <?php
 
-/*
-create table choferes_vehiculos(
-  chofer int,
-  vehiculo int,
-  primary key (chofer, vehiculo),  
-  foreign key (chofer) references choferes(id),
-  foreign key (vehiculo) references vehiculos(id)
-);
-*/
-
+require_once '../db/db.php';
 class choferes_vehiculos
 {
-  private $chofer;
-  private $vehiculo;
+  public $chofer;
+  public $vehiculo;
 
-  public function __construct($chofer, $vehiculo)
+  public function __construct($chofer=null, $vehiculo=null)
   {
     $this->chofer = $chofer;
     $this->vehiculo = $vehiculo;
   }
 
-  // public function insertChoferVehiculo($chofer, $vehiculo)
-  // public function updateChoferVehiculo($chofer, $vehiculo)
-  // public function deleteChoferVehiculo($chofer, $vehiculo)
-  // public function getChoferesVehiculos($chofer=null, $vehiculo=null)
-  // public function validateChoferVehiculo($chofer, $vehiculo)
+  public function insertChoferVehiculo($chofer, $vehiculo)
+  {
+    global $db;
+    $query = "INSERT INTO choferes_vehiculos (chofer, vehiculo) VALUES (?, ?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ii", $chofer, $vehiculo);
+    if ($stmt->execute()) {
+      return $db->insert_id;
+    } else {
+      return false;
+    }
+  }
+  public function updateChoferVehiculo($chofer, $vehiculo)
+  {
+    global $db;
+    $query = "UPDATE choferes_vehiculos SET vehiculo=? WHERE chofer=?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ii", $vehiculo, $chofer);
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public function deleteChoferVehiculo($chofer, $vehiculo)
+  {
+    global $db;
+    $query = "UPDATE FROM choferes_vehiculos SET is_active=false WHERE chofer=? AND vehiculo=?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("ii", $chofer, $vehiculo);
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public function getChoferesVehiculos($chofer = null, $vehiculo = null)
+  {
+    global $db;
+    $choferes_vehiculos = array();
+    if (!$chofer && !$vehiculo) {
+      $query = "SELECT * FROM choferes_vehiculos WHERE is_active=true";
+      $stmt = $db->prepare($query);
+    }
+    if ($chofer && !$vehiculo) {
+      $query = "SELECT * FROM choferes_vehiculos WHERE chofer=? AND is_active=true";
+      $stmt = $db->prepare($query);
+      $stmt->bind_param("i", $chofer);
+    }
+    if (!$chofer && $vehiculo) {
+      $query = "SELECT * FROM choferes_vehiculos WHERE vehiculo=? AND is_active=true";
+      $stmt = $db->prepare($query);
+      $stmt->bind_param("i", $vehiculo);
+    }
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+      $choferes_vehiculos[] = $row;
+    }
+    return $choferes_vehiculos;
+  }
 }
